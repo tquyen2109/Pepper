c4227a3cffbff075a3d8592f240eabb933121176
Quyen Tran <t.quyen1997@yahoo.com>
2020-01-29 22:01:55 -0500

    App Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {AngularFireDatabase,AFireList} from '@angular/fire/database';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pepper';
+  cuisines: Observable<any[]> ;
+  restaurant;
+  constructor(private af: AngularFireDatabase) {
+    
+    
+  }
+  ngOnInit(){
+    this.cuisines = this.af.list('cuisines').valueChanges();
+    this.restaurant = this.af.object('/restaurant').valueChanges();
+  }
+ add(){
+   this.cuisines.
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {AngularFireModule} from '@angular/fire'
+import { AngularFireDatabase } from '@angular/fire/database';
+export const firebaseConfig =  {
+  apiKey: 'AIzaSyCS55KcK2iYQKbPAQKmWZ2n7Vfank7ZwxM',
+  authDomain: 'pepper-a11f2.firebaseapp.com',
+  databaseURL: 'https://pepper-a11f2.firebaseio.com',
+  projectId: 'pepper-a11f2',
+  storageBucket: 'pepper-a11f2.appspot.com',
+  messagingSenderId: '990893562965',
+  appId: '1:990893562965:web:17e5812aae7fb8329b08dc',
+  measurementId: 'G-LHPC81KNEL'
+}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [AngularFireDatabase],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
